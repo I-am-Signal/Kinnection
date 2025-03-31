a71939c56767787d887b03cb3d7df9ae4960be0e
Dylan Carder <wcarder1@students.kennesaw.edu>
2025-03-31 16:29:30 -0400

    Add GET /users/{id} testcase

--- Kinnection-Backend/test/tests/UsersTest.cs
+++ Kinnection-Backend/test/tests/UsersTest.cs
@@ -115,6 +115,38 @@ public class UsersTest
         Assert.That(output["email"].GetString(), Is.EqualTo(RequestContent["email"]));
     }
 
+    [Test, Order(3)]
+    public async Task PosGetUsers()
+    {
+        // Make request
+        var Header = new Dictionary<string, string>()
+        {
+            ["Authorization"] = $"Bearer {TestRunner.Access}",
+            ["X-Refresh-Token"] = TestRunner.Refresh
+        };
+    
+        HttpResponseMessage Response = await HttpService.GetAsync(
+            URI + UserSubDir,
+            Parameter: $"{UserInfo["id"].GetInt32()!}",
+            Headers: Header
+        );
+
+        // Ensure expected status code
+        Assert.That(Response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+
+        // Verify and save tokens
+        TestRunner.CheckTokens(Response.Headers);
+        TestRunner.SaveTokens(Response.Headers);
+
+        // Evaluate content
+        var output = JsonSerializer.Deserialize<Dictionary<string, JsonElement>>(
+            await Response.Content.ReadAsStringAsync());
+
+        Assert.That(output!["id"].GetInt32(), Is.EqualTo(UserInfo["id"].GetInt32()));
+        Assert.That(output["fname"].GetString(), Is.EqualTo(UserInfo["fname"].GetString()));
+        Assert.That(output["lname"].GetString(), Is.EqualTo(UserInfo["lname"].GetString()));
+        Assert.That(output["email"].GetString(), Is.EqualTo(UserInfo["email"].GetString()));
+    }
     [OneTimeTearDown]
     public void TearDown() { }
 }
