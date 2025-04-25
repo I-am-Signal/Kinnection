d3b2535ec111490a09b751bca4e2f3f5817f920e
Dylan Carder <wcarder1@students.kennesaw.edu>
2025-04-25 17:38:22 -0400

    Add optional -e /path argument to GetEnvVars PowerShell and Shell scripts

--- Kinnection-Backend/app/GetEnvVars.ps1
+++ Kinnection-Backend/app/GetEnvVars.ps1
@@ -2,11 +2,18 @@
 # Copies environment variables from the Docker directory to run ASP.NET Core
 # app locally. 
 #
-# NOTE: Must navigate to 'Kinnection/Kinnection-Backend/app' before use
-# To execute in Powershell, use './GetEnvVars.ps1'
+# Args:
+#   -e /path/to/.env
+#
+# If using without -e argument, the terminal must be navigated to 
+# 'Kinnection/Kinnection-Backend/app' before use.
+# To execute in Powershell, use './GetEnvVars.ps1 -e /path/to/.env'
 
 # Load environment variables from the .env file
-$envFile = "..\..\Docker\.env"
+param( [string]$e )
+
+$envFile = "..\Docker\.env"
+if ($e) { $envFile = $e }
 
 # Read the .env file line by line and set environment variables
 Get-Content $envFile | ForEach-Object {
--- Kinnection-Backend/app/GetEnvVars.sh
+++ Kinnection-Backend/app/GetEnvVars.sh
@@ -1,19 +1,40 @@
 #!/bin/bash
 # GetEnvVars.sh
 # Copies environment variables from the Docker directory to run ASP.NET Core
-# app locally.
+# app locally. 
 #
-# NOTE: Must navigate to 'Kinnection/Kinnection-Backend/app' before use
-# To execute in Bash, use './GetEnvVars.sh'
+# Args:
+#   -e /path/to/.env
+#
+# If using without -e argument, the terminal must be navigated to 
+# 'Kinnection/Kinnection-Backend/app' before use.
+# To execute in Bash, use './GetEnvVars.sh -e /path/to/.env'
 
 # Load environment variables from the .env file
 envFile="../../Docker/.env"
+while getopts "e:" opt; do
+    case $opt in
+        e)
+            envFile="$OPTARG"
+            ;;
+    esac
+done
+            
 
 # Read the .env file line by line and set environment variables
 while IFS='=' read -r envName envValue; do
-    # Skip empty lines or lines starting with a comment
-    if [[ -n "$envName" && ! "$envName" =~ ^# ]]; then
-        export "$envName"="$envValue"
+    # Trim whitespace
+    envName="$(echo "$envName" | xargs)"
+    envValue="$(echo "$envValue" | xargs)"
+
+    # Skip comments only
+    if [[ "$envName" == \#* || -z "$envName" ]]; then
+        continue
+    fi
+
+    # Export even if envValue is empty
+    if [[ "$envName" =~ ^[A-Za-z_][A-Za-z0-9_]*$ ]]; then
+        export "$envName=$envValue"
     fi
 done < "$envFile"
 
